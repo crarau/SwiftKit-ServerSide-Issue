af52e0c7654a7c81290ced445ebb69a9833aeb80
Ciprian Rarau <ciprarau@gmail.com>
2017-05-31 22:24:15 -0400

    Simplified code

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,22 @@
+import Foundation
 import Kitura
+import PromiseKit
+
+class PromiseKitServerSide {
+    func test(completion:@escaping (String)->()) {
+        print("in test")
+        firstly {
+            return Promise.init(value: "promise")
+            }.then { value in
+                print("in then")
+                completion(value)
+            }.always {
+                completion("done!");
+        }
+        
+    }
+    
+}
 
 // Create a new router
 let router = Router()
@@ -6,9 +24,14 @@ let router = Router()
 // Handle HTTP GET requests to /
 router.get("/") {
     request, response, next in
-    response.send("Hello, World!")
     
-    PromiseKitServerSide().test()
+    PromiseKitServerSide().test(completion: { (result) in
+        response.send(result)
+    })
+    
+    RunLoop.main.run()
+    
+    response.send("done")
     next()
 }
 
@@ -17,3 +40,5 @@ Kitura.addHTTPServer(onPort: 8888, with: router)
 
 // Start the Kitura runloop (this call never returns)
 Kitura.run()
+
+
