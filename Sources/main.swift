aed08139945ed1e028cdce905f9ab1b57880afa5
Ciprian Rarau <ciprarau@gmail.com>
2017-05-31 22:31:29 -0400

    Simplified code

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,6 +5,7 @@ import PromiseKit
 class PromiseKitServerSide {
     func test(completion:@escaping (String)->()) {
         print("in test")
+        
         firstly {
             return Promise.init(value: "promise")
             }.then { value in
@@ -13,32 +14,21 @@ class PromiseKitServerSide {
             }.always {
                 completion("done!");
         }
-        
+
     }
     
 }
 
-// Create a new router
 let router = Router()
-
-// Handle HTTP GET requests to /
-router.get("/") {
-    request, response, next in
-    
+router.get("/") { request, response, next in
     PromiseKitServerSide().test(completion: { (result) in
         response.send(result)
     })
-    
     RunLoop.main.run()
-    
-    response.send("done")
-    next()
 }
 
-// Add an HTTP server and connect it to the router
+print("starting server")
 Kitura.addHTTPServer(onPort: 8888, with: router)
-
-// Start the Kitura runloop (this call never returns)
 Kitura.run()
 
 
